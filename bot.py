7a388c36948711c75508b218b651989fdfa73cf8
Kureichi <hibikumonogatari@gmail.com>
2025-05-17 16:59:45 +0700

    fix routing

--- bot.py
+++ bot.py
@@ -5,11 +5,15 @@ from flask import Flask, request
 from handlers import message_handler, callback_handler
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
+bot.remove_webhook()
+bot.set_webhook(url=config.WEBHOOK_URL)
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route(config.PATH, methods=['POST'])
 def webhook():
+    print('Incoming message from telegram..')
+
     json = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json)
 
--- config.py
+++ config.py
@@ -5,4 +5,5 @@ import os
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 USER_CHAT_ID = os.getenv('USER_CHAT_ID')
-WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+PATH = os.getenv('PATH', '/webhook')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL') + PATH
