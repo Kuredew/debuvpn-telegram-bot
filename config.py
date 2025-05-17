1ff03a40b2216005efc923740a5451fbc63be575
Kureichi <hibikumonogatari@gmail.com>
2025-05-17 17:35:28 +0700

    nah

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 #load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-USER_CHAT_ID = os.getenv('USER_CHAT_ID')
+USER_CHAT_ID = int(os.getenv('USER_CHAT_ID'))
 PATH = '/webhook'
 WEBHOOK_URL = os.getenv('WEBHOOK_URL') + PATH
